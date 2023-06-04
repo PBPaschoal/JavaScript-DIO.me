44db2b895d6b85eb1f21235694df025ef0ffb29a
PBPaschoal <programming.paschoal@gmail.com>
2023-06-03 23:47:39 -0300

    Finalizando a aula 02

--- Bootcamp/Formação JavaScript Developer - Dio.me/Aprenda a Programar do Zero com JavaScript/Curso 01 - Primeiros Passos com JavaScript/Aula 02 - Estruturas Condicionais/aula02.js
+++ Bootcamp/Formação JavaScript Developer - Dio.me/Aprenda a Programar do Zero com JavaScript/Curso 01 - Primeiros Passos com JavaScript/Aula 02 - Estruturas Condicionais/aula02.js
@@ -1,5 +1,7 @@
 // Estruturas Condicionais
 // Exemplo: Pessoa 1 usa a camisa azul e pessoa 2 usa a camisa vermelha
+// OBS: Fiz um pouco diferente das aulas:
+
 const camisetaP1Azul = true;
 const camisetaP2Azul = false;
 
@@ -8,7 +10,9 @@ console.log(camisetaP2Azul);
 
 const number = 29;
 const result = number % 2;
-console.log("=============================");
+
+console.log('=============================');
+
 if (result === 0) { // O operador === ele não faz comparação de uma variavel número comparado com string, sempre será false. Exemplo: numero % 2 === '0'; FALSE. 
     console.log('Number ' + number + ' is even!');
 } else {
@@ -19,7 +23,8 @@ if (result === 0) { // O operador === ele não faz comparação de uma variavel 
 
 const numero = 3;
 const numeroPar = (numero % 2) === 0;
-console.log("=============================");
+
+console.log('=============================');
 console.log('Number ' + numero + ' is ' + numeroPar);
 
 if (numeroPar) {
@@ -31,7 +36,9 @@ if (numeroPar) {
 // ou
 
 const imparOUpar = 5;
-console.log("=============================");
+
+console.log('=============================');
+
 if ((imparOUpar % 2) === 0) {
     console.log('O número é par');
 } else {
@@ -42,7 +49,8 @@ if ((imparOUpar % 2) === 0) {
 
 const umNumero = 25;
 const numberdivpor = (umNumero % 5) === 0;
-console.log("=============================");
+
+console.log('=============================');
 
 if (umNumero === 0) {
     console.log('O número é inválido!');
@@ -54,7 +62,23 @@ if (umNumero === 0) {
     console.log('Não!');
 }
 
-// Desafio da aula passada, mas com incremento
+// Desafio da aula passada com incremento
 const gasolinePrice = 5.21;
 const ethanolPrice = 4.10;
+
+let carFuel = `Ethanol`;
+let averageSpending = 14;
+let distanceTravel = 100;
+let amountSpent;
+
+const litersConsumed = distanceTravel / averageSpending;
+
+if (carFuel === `Gasoline`) {
+    amountSpent = litersConsumed * ethanolPrice;
+} else {
+    amountSpent = litersConsumed * gasolinePrice;
+}
+
+console.log('=============================');
+console.log('$ ', amountSpent.toFixed(2));
 
